450e4f07d4e4d421c709267f37742b55dcb69d3d
Uwe Jaekel <uja.works@gmail.com>
2024-02-04 15:20:17 +0100

    uja with copilot

--- uja/init.el
+++ uja/init.el
@@ -15,7 +15,11 @@
 
 ;; Setting a theme
 ;(load-theme 'modus-vivendi t)
+;(load-theme 'modus-operandi t)
 (load-theme 'deeper-blue t)
+;(load-theme 'tsdh-light t)
+;(load-theme 'tsdh-dark t)
+
 
 ;; Set the title
 (setq frame-title-format "Emacs @ %b")
@@ -31,15 +35,24 @@
 
 
 
-;; Yasnippets
+;;Yasnippets
 (use-package yasnippet
   :ensure t
   :config
-  (yas-global-mode 1))
+  (yas-global-mode 1)
+  :init
+  (setq yas-snippet-dirs '("~/Emacs/uja_snippets"))
+  )
 
-(setq yas-snippet-dirs '("Emacs/uja_snippets"))
 
-
+;; (use-package yasnippet
+;;   :ensure t
+;;   :hook ((text-mode
+;;           prog-mode
+;;           conf-mode
+;;           snippet-mode) . yas-minor-mode-on)
+;;   :init
+;;   (setq yas-snippet-dir "~/.emacs.d/snippets"))
 
 ;; AUCTeX
 (use-package tex
@@ -55,7 +68,7 @@
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
 ;; Enable TeX-fold-mode by default in AUCTeX (optional)
-(add-hook 'TeX-mode-hook '(lambda () (TeX-fold-mode 1)))
+(add-hook 'TeX-mode-hook #'(lambda () (TeX-fold-mode 1)))
 
 ;; AUCTeX configuration for Skim as PDF viewer
 (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
@@ -78,12 +91,25 @@
 
 
 ;; Org Config
+
+(require 'ob-latex)
+(with-eval-after-load 'org
+  (setq org-latex-create-formula-image-program 'imagemagick)
+  (setq org-latex-packages-alist
+        '(("" "newpxtext,newpxmath" t)
+          ("" "tikz" t)
+          ("" "tikz-cd" t)
+          ("" "eulervm" t)))
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.6)))
+  
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)
    (julia . t)
    (dot . t)
-   (lisp . t))) ;; for Common Lisp
+   (lisp . t)
+   (latex . t)))
 
 ;; Safety: set to t to ask for confirmation before executing code blocks
 (setq org-confirm-babel-evaluate nil)
@@ -92,15 +118,71 @@
 (require 'org-tempo)
 (add-to-list 'org-modules 'org-tempo t)
 
+
+(add-to-list 'org-src-lang-modes '("lisp" . sly))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company sly auctex yasnippet)))
+ '(package-selected-packages
+   '(copilot quelpa-use-package quelpa editorconfig dash s company sly auctex yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;;; Evil mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+
+(use-package s
+  :ensure t)
+
+(use-package dash
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t)
+
+(use-package quelpa
+  :ensure t)
+
+(use-package quelpa-use-package
+  :ensure t)
+
+;;; Copilot
+(use-package copilot
+  :quelpa (copilot :fetcher github
+                   :repo "copilot-emacs/copilot.el"
+                   :branch "main"
+                   :files ("dist" "*.el")
+		   :config (copilot-mode -1)))
+  
+;; you can utilize :map :hook and :config to customize copilot
+;;
+(add-hook 'prog-mode-hook 'copilot-mode)
+;;
+(with-eval-after-load 'company
+  ;; disable inline previews
+  (delq 'company-preview-if-just-one-frontend company-frontends))
+
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
+
+;; switch off copilot mode
+;;(copilot-mode -1)
+;; switch on copilot mode
+;;(copilot-mode 1)
+
+(load "~/Dropbox/Dropsync/Emacs/uja.el")
+
---
+++ uja_snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+		     '(("!vl" "#+title: $(1:titel)\n#+author: Uwe Jaekel\n#+language: de\n#+options: toc:nil num:nil\n#+latex_header: \\usepackage{amsmath}\n#+latex_header: \\usepackage{amssymb}\n#+latex_header: \\usepackage{mathtools}\n#+latex_header: \\usepackage{tikz}\n# a4wide\n#+latex_header: \\usepackage{geometry}\n#+latex_header: \\geometry{a4paper, top=25mm, left=25mm, right=25mm, bottom=25mm, headsep=10mm, footskip=12mm}\n# Deutsches Trennmuster\n#+latex_header: \\usepackage[ngerman]{babel}\n\n$0" "Vorlesung" nil nil nil "/Users/uwejaekel/Emacs/uja_snippets/org-mode/vorlesung" nil nil)))
+
+
+;;; Do not edit! File generated at Sat Dec  9 01:02:52 2023
